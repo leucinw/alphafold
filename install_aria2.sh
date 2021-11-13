ace65c986f5d3acc6ba95c100d27cc6e42bbd5a0
Chengwen Liu <liuchw2010@gmail.com>
2021-11-13 11:11:55 -0600

    install aria2 for downloading data

---
+++ install_aria2.sh
@@ -0,0 +1,9 @@
+#Download latest epel-release rpm from
+
+ http://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/
+
+# Install epel-release rpm:
+rpm -Uvh epel-release*rpm
+
+# Install aria2 rpm package:
+ dnf install aria2
