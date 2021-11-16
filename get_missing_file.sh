cbf90d09d25b155118fc803c6ca99085d890e659
Chengwen Liu <liuchw2010@gmail.com>
2021-11-16 11:31:00 -0600

    remove unnecessary file

--- get_missing_file.sh
+++
@@ -1,2 +0,0 @@
-wget https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt 
-mv stereo_chemical_props.txt ./alphafold/common
