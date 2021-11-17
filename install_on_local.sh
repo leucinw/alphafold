a25d866e6f0a1f1731515bcbfff494df68683706
Chengwen Liu <liuchw2010@gmail.com>
2021-11-16 20:20:30 -0600

    detailed installation settings

--- install_on_local.sh
+++ install_on_local.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# This file is adapted from github.com/kuixu
+# This file was adapted from github.com/kuixu
 
-# conda pkgs
-#conda create -n alphafold2 python=3.8 -y
-#conda activate alphafold2
-#
-##
-## version number of cudnn has been modified.. 
-## check here: https://developer.nvidia.com/rdp/cudnn-archive
-## 
+# create alphafold2_cu11 environment 
+#conda create -n alphafold2_cu11 python=3.8 -y
+#conda activate alphafold2_cu11
 
-wget https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt 
-mv stereo_chemical_props.txt ./alphafold/common
+# create alphafold2_cu10 environment 
+#conda create -n alphafold2_cu10 python=3.8 -y
+#conda activate alphafold2_cu10
+
+#wget https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt 
+#mv stereo_chemical_props.txt ./alphafold/common
+
+# cudnn version number: https://developer.nvidia.com/rdp/cudnn-archive
 
 conda install -y -c conda-forge cudnn=8.1.0
 conda install -y -c bioconda hmmer hhsuite==3.3.0 kalign2
@@ -22,19 +23,21 @@ conda install -y -c conda-forge \
       pdbfixer \
       pip
 
-# python pkgs
+# for cuda 11.2
+#pip3 install --upgrade pip \
+#    && pip3 install -r ./requirements.txt \
+#    && pip3 install --upgrade "jax[cuda111]" -f \
+#    https://storage.googleapis.com/jax-releases/jax_releases.html \
+#    && pip3 install jaxlib==0.1.69+cuda111 -f \
+#    https://storage.googleapis.com/jax-releases/jax_releases.html
+
+# for cuda10.2
 pip3 install --upgrade pip \
     && pip3 install -r ./requirements.txt \
-    && pip3 install --upgrade "jax[cuda111]" -f \
+	  && pip3 install --upgrade "jax[cuda102]" -f \
     https://storage.googleapis.com/jax-releases/jax_releases.html \
-    && pip3 install jaxlib==0.1.70+cuda111 -f \
+    && pip3 install jaxlib==0.1.69+cuda102 -f \
     https://storage.googleapis.com/jax-releases/jax_releases.html
-
-# or cuda10.2
-	 #&& pip3 install --upgrade "jax[cuda102]" -f \
-    #https://storage.googleapis.com/jax-releases/jax_releases.html \
-    #&& pip3 install jaxlib==0.1.70+cuda102 -f \
-    #https://storage.googleapis.com/jax-releases/jax_releases.html
 
 work_path=`pwd`
 # update openmm 
@@ -42,6 +45,5 @@ a=$(which python)
 cd $(dirname $(dirname $a))/lib/python3.8/site-packages
 patch -p0 < $work_path/docker/openmm.patch
 
-#
-# see env_cuda10.2.txt for detailed settings 
-#
+# see alphafold2_cu10.yml for cuda10.2 settings 
+# see alphafold2_cu11.yml for cuda11.2 settings 
